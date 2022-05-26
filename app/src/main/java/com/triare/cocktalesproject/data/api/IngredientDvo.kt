1005306d901055ea13d0e223ac3ab7e4a3ba3c52
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2022-05-26 17:01:15 +0300

    26.05

--- app/src/main/java/com/triare/cocktalesproject/data/api/IngredientDvo.kt
+++
@@ -1,8 +0,0 @@
-package com.triare.cocktalesproject.data.api
-
-data class   Ingredients(val ingredient: IngredientDvo)
-
-data class IngredientDvo(
-      val name: String,
-      val picture:String
-)
