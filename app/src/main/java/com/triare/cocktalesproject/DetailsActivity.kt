7959b20a70feceee66d7b6b9dd236691eac4186a
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2022-05-27 15:27:53 +0300

    ActionBar

--- app/src/main/java/com/triare/cocktalesproject/DetailsActivity.kt
+++ app/src/main/java/com/triare/cocktalesproject/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.triare.cocktalesproject
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,19 +21,34 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        ("БЕБРА")
         viewModel = DetailsViewModel(getId())
         val recyclerView: RecyclerView = binding.recycleViewIngredients
         recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
         ingredientAdapter = IngredientAdapter(applicationContext)
         recyclerView.adapter = ingredientAdapter
         viewModel._cocktaleDvoLiveData.observe(this) {
-            binding.nameAlcohol.text = it.name
+            setTitle(it.name)
+           // binding.nameAlcohol.text = it.name
+
             binding.descriptionAlcohol.text = "    " + it.instruction
             Glide.with(this).load(it.picture).error(R.drawable.ic_launcher_background)
                 .into(binding.imageAlcohol)
             ingredientAdapter.setDataList(it.ingredients)
         }
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     private fun getId(): Int {
         val arguments = intent.extras
